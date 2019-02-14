781bb0c1712ccef54ea4a9fcaf7fa81a9b58b444
Rebecca Jackson <zimbecs@gmail.com>
2019-02-15 09:31:57 +1300

    removing unnecessary components from app

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Banner from './images/banner.jpg';
+// import Banner from './images/banner.jpg';
 import './style.scss';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="header">
-        <a href="https://twitter.com/flexdinesh">
-          <img src={Banner} alt="react-redux-boilerplate - Logo" />
-        </a>
+        <h1>Fix Me!</h1>
+        <h3>I is broke</h3>
+        
         <div className="nav-bar">
           <Link className="router-link" to="/">
             Home
-          </Link>
-          <Link className="router-link" to="/features">
-            Features
           </Link>
         </div>
       </div>
