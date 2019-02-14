9a43d26a66365a5c99abe5a6a23536b81bfe198d
Rebecca Jackson <zimbecs@gmail.com>
2019-02-15 09:53:42 +1300

    small change to helmet in app

--- app/containers/HomePage/HomePage.js
+++ app/containers/HomePage/HomePage.js
@@ -7,7 +7,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
-// import ReposList from 'components/ReposList';
 import './style.scss';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -25,10 +24,20 @@ export default class HomePage extends React.PureComponent { // eslint-disable-li
     const {
       loading, error, repos, username, onChangeUsername, onSubmitForm
     } = this.props;
- 
+    // const reposListProps = {
+    //   loading,
+    //   error,
+    //   repos
+    // };
+
     return (
       <article>
-     
+       <Helmet>
+          <title>Home Page</title>
+          <meta name="description" content="A React.js Boilerplate application homepage" />
+        </Helmet>
+   
+  
       </article>
     );
   }
